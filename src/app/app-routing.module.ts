74dcef7f9fad7ca78e6fe22fb88f7166b1594083
adrianohenriquess <adriano.henriquesds@gmail.com>
2021-01-11 12:29:27 -0300

    Criando os componentes de diversão e restaurantes e adicionando rotas para os componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { DiversaoComponent } from './diversao/diversao.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+export const ROUTES: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'restaurantes', component: RestaurantesComponent},
+  { path: 'diversao', component: DiversaoComponent}
+];
 
-@NgModule({
+/*@NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { }*/
