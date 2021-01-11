ba540d36d1f9f9c0141033e48364133b74587f05
adrianohenriquess <adriano.henriquesds@gmail.com>
2021-01-11 15:08:29 -0300

    adicionando o link de orfertas para os componentes home, diversao e restaurantes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OfertaComponent } from './oferta/oferta.component';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent},
   { path: 'restaurantes', component: RestaurantesComponent},
-  { path: 'diversao', component: DiversaoComponent}
+  { path: 'diversao', component: DiversaoComponent},
+  { path: 'oferta', component: OfertaComponent},
+  { path: 'oferta/:id', component: OfertaComponent}
 ];
 
 /*@NgModule({
